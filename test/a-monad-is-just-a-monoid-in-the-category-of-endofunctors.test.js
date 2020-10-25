2326352bfd661f11f43994de596c77e5d492f3c1
York Huang <you@example.com>
2020-10-25 14:53:58 +1100

    clean up

--- test/a-monad-is-just-a-monoid-in-the-category-of-endofunctors.test.js
+++ test/a-monad-is-just-a-monoid-in-the-category-of-endofunctors.test.js
@@ -1,28 +1,30 @@
-const { expect } = require('chai');
+const { assert: { deepEqual } } = require('chai');
 const curry = (f, arity = f.length, ...args) => arity <= args.length ? f(...args) : (...a) => curry(f, arity, ...args, ...a);
 const id = x => x;
 const compose = curry((g, f) => x => g(f(x)));
+const composeK = curry((g, f) => compose(chain(g), f));
+const map = curry((f, xs) => xs.map(f));
+const chain = curry((f, xs) => xs.flatMap(f));
+const unit = x => Array.of(x);
+
 
 describe(`A Monad is just a Monoid in the category of Endofunctors, what's the problem?`, () => {
   describe('Array', () => {
-    const map = curry((f, xs) => xs.map(f));
-    const chain = curry((f, xs) => xs.flatMap(f));
-    const unit = x => Array.of(x);
 
     describe('is a functor', () => {
       it('follows identity law', () => {
         const m = unit(3);
-        expect(
-          map(id, m)
-        ).deep.equal(m);
+        deepEqual(
+          map(id, m),
+          m
+        );
       });
       it('follows composition law', () => {
         const m = unit(3);
         const f = x => x + 10;
         const g = x => x * 3;
-        expect(
-          compose(map(g), map(f))(m)
-        ).to.deep.equal(
+        deepEqual(
+          compose(map(g), map(f))(m),
           map(compose(g, f))(m)
         );
       });
@@ -31,18 +33,15 @@ describe(`A Monad is just a Monoid in the category of Endofunctors, what's the p
       it('follows left identity law', () => {
         const x = 3;
         const f = x => unit(x + 10);
-
-        expect(
-          chain(f, unit(x))
-        ).to.deep.equal(
+        deepEqual(
+          chain(f, unit(x)),
           f(x)
         );
       });
       it('follows right identity law', () => {
         const m = unit(3);
-        expect(
-          chain(unit, m)
-        ).to.deep.equal(
+        deepEqual(
+          chain(unit, m),
           m
         );
       });
@@ -50,34 +49,28 @@ describe(`A Monad is just a Monoid in the category of Endofunctors, what's the p
         const m = unit(3);
         const f = x => unit(x + 10);
         const g = x => unit(x * 3);
-        expect(
-          compose(chain(g), chain(f))(m)
-        ).to.deep.equal(
+        deepEqual(
+          compose(chain(g), chain(f))(m),
           chain(x => chain(g, f(x)))(m)
         );
       });
     });
     describe('is a monoid in the category of endofunctors', () => {
-      const concat = (g, f) =>
-        compose(
-          chain(g),
-          f
-        );
+      const concat = composeK;
+      const empty = unit;
       it('follows left identity law', () => {
         const x = 3;
-        const f = x => unit(x + 10);
-        expect(
-          concat(unit, f)(x)
-        ).to.deep.equal(
+        const f = x => empty(x + 10);
+        deepEqual(
+          concat(empty, f)(x),
           f(x)
         );
       });
       it('follows right identity law', () => {
         const x = 3;
         const f = x => unit(x + 10);
-        expect(
-          concat(f, unit)(x)
-        ).to.deep.equal(
+        deepEqual(
+          concat(f, empty)(x),
           f(x)
         );
       });
@@ -86,9 +79,8 @@ describe(`A Monad is just a Monoid in the category of Endofunctors, what's the p
         const f = x => unit(x + 10);
         const g = x => unit(x * 3);
         const h = x => unit(x ^ 2);
-        expect(
-          concat(concat(f, g), h)(x)
-        ).to.deep.equal(
+        deepEqual(
+          concat(concat(f, g), h)(x),
           concat(f, concat(g, h))(x)
         );
       });
